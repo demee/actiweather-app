ed720998b4313e1b8835cebe32c5eb0eaa34eb39
Slawomir Demichowicz <slawomir.demichowicz@gmail.com>
2020-09-22 23:43:49 +0100

    Added application and static route

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ public/precache-manifest.3a5451ab49709e8c69e5e1e07dc7b6fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd28662ba5d43107213fa4d813f82a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a3b88c6021dcfcee6b8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7d6e16c526f08ae58367",
+    "url": "/static/js/2.00d9c745.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00d9c745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a3b88c6021dcfcee6b8",
+    "url": "/static/js/main.ddf8ba90.chunk.js"
+  },
+  {
+    "revision": "85bae7b30fc954a896ec",
+    "url": "/static/js/runtime-main.f3e66c10.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
