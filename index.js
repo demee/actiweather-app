e6bf9243c9e107bec8c1d97f041f3dd90bacc74b
Slawomir Demichowicz <slawomir.demichowicz@gmail.com>
2020-09-22 23:46:14 +0100

    Remove __dirname

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import path from 'path';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static('public'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
