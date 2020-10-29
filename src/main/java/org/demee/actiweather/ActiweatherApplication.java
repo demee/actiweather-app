6b71fa2760359eb14c57686f006dec3d45663eda
demee <slawomir.demichowicz@gmail.com>
2020-10-29 11:44:18 +0000

    Added spring boot application

---
+++ src/main/java/org/demee/actiweather/ActiweatherApplication.java
@@ -0,0 +1,13 @@
+package org.demee.actiweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiweatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/demee/actiweather/ActiweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demee.actiweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
